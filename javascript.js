2d435d9350ffeae8e2cd37e1e0b96966f6a2d1c4
veggie-group <40884653+veggie-group@users.noreply.github.com>
2018-07-06 11:32:08 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,3 +4,14 @@ $( document ).ready(function() {
 	
     $("#main_title").text("My Title!")
 });
+
+
+function recalc() {
+  var months =Number($("#months").val()) + Number($("#years").val()) * 12;
+  $("#water").text((months) * 124917);
+  $("#forest").text((months) * 84);
+  $("#lives").text(months * 30);
+  $("#grain").text(months * 543);
+  $("#Co2").text(months * 273);
+
+}
