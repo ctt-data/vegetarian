6242b211f4bbf06ef9df6e0190a6721d16216648
veggie-group <40884653+veggie-group@users.noreply.github.com>
2018-07-06 11:37:45 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 
-$( document ).ready(function() {
+function recalc() {
   var months =Number($("#months").val()) + Number($("#years").val()) * 12;
   $("#water").text((months) * 124917);
   $("#forest").text((months) * 84);
   $("#lives").text(months * 30);
   $("#grain").text(months * 543);
   $("#Co2").text(months * 273);
-
 }
 
 	// All your code goes here
